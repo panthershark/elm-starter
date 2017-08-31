4e2878e0a5bf1533b4921eafaff6f4e1ff6ce6f1
Tommy Messbauer <totally.bro@gmail.com>
2017-08-31 18:58:51 -0500

    add docs so exposed module doesn't explode.

--- src/elm/Hello.elm
+++ src/elm/Hello.elm
@@ -1,10 +1,18 @@
 module Hello exposing (view)
 
+{-| This is a demo of a parrot component which shows the text of an input field in the title
+
+@docs view
+
+-}
+
 import Html exposing (..)
 import Html.Events exposing (onInput)
 import Html.Attributes exposing (defaultValue, class)
 
 
+{-| Renders the hello view
+-}
 view : String -> (String -> msg) -> Html msg
 view str msgInput =
     let
